d4cab18b9488bf01c635ab342e334bb850a3cbaa
Gabriel S. Dias <102713815+Staytof@users.noreply.github.com>
2025-07-27 16:33:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const slides = document.querySelector('.slides-container');
+const slide = document.querySelectorAll('.slide');
+let currentIndex = 0;
+
+document.querySelector('.next').addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % slide.length;
+  updateSlider();
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + slide.length) % slide.length;
+  updateSlider();
+});
+
+function updateSlider() {
+  slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
